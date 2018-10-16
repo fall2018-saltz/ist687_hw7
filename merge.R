3103a761c4f9b216562c8d4cc2dbce8a7720be08
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:26:09 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,1 +1,13 @@
 
+
+#copying USarrests data in a variable
+arrests <- USArrests
+#creating a column named stateName
+arrests$stateName<-rownames(arrests)
+str(arrests)
+#Merging the data
+final_data <-merge(clean_data,arrests,by="stateName")
+
+str(final_data)
+#str(clean_data)
+#str(arrests)
