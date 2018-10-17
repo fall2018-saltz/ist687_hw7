4de573ae093fd4f0684d8594109488b3818695c3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:39:50 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -22,7 +22,7 @@ map.pop <- ggplot(final_data,aes(x=x,y=y,map_id= stateName,color='red',size=popu
                 coord_map()+
                 geom_point()
               
-NY <- geocode(source='dsk',"New York,ny")
+NY <- geocode("New York,ny")
 
 map.ny <-
  
