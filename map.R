a4cc9834e64e514e736b331e62fecb3946304d02
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:22:50 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -6,5 +6,6 @@ str(final_data)
 map.simple <- ggplot(final_data,aes(map_id= stateName))+
                 geom_map(map=usa,aes(fill=area))+
                 expand_limits(x=final_data$x,y=final_data$y)+
-                coord_map()+scale_fill_gradient(low="blue", high="red")
+                coord_map()+
+                scale_fill_gradient(low="blue", high="red")
 map.simple
