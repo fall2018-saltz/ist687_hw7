b985453d6b68ae5caebac32062cddfaf530845e1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:29:14 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -16,4 +16,10 @@ map.murder <- ggplot(final_data,aes(map_id= stateName))+
                 scale_fill_gradient(low="blue", high="red")
 
 
+map.pop <- ggplot(final_data,aes(map_id= stateName))+
+                geom_map(map=usa,aes(size=Population))+
+                expand_limits(x=final_data$x,y=final_data$y)+
+                coord_map()
+              
+
  
