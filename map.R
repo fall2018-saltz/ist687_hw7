9d7d8cfed91f877047a8ec326754083e2db0d511
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:23:39 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -3,9 +3,9 @@ library(ggplot2)
 usa <- map_data('state')
 final_data$stateName <- tolower(final_data$stateName)
 str(final_data)
-map.simple <- ggplot(final_data,aes(map_id= stateName))+
+map.area <- ggplot(final_data,aes(map_id= stateName))+
                 geom_map(map=usa,aes(fill=area))+
                 expand_limits(x=final_data$x,y=final_data$y)+
                 coord_map()+
                 scale_fill_gradient(low="blue", high="red")
-map.simple
+map.area
