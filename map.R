e1ffa76bca9a24bd676278024b5cff122b0a6fd8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:16:47 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -4,5 +4,5 @@ usa <- map_data('state')
 final_data$stateName <- tolower(final_data$stateName)
 str(final_data)
 map.simple <- ggplot(final_data,aes(map_id= state))+
-                geom_map(map=us,aes(fill=area))
+                geom_map(map=usa,aes(fill=area))
 map.simple
