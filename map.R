fa96023a98c1f18aa88274c217d6fd0f5b55f085
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:20:02 -0400

    Saving map.R

--- map.R
+++ map.R
@@ -36,11 +36,11 @@ map.murder.northeast <- ggplot(final_data,aes(map_id= stateName))+
                          ylim(NY$lat-10,NY$lat+10)  
                  
 
-map.pop.northeast <- ggplot(final_data,aes(x=x,y=y,map_id= stateName,color='red',size=population))+
+map.pop.northeast <- ggplot(final_data,aes(x=x,y=y,map_id= stateName,size=population))+
                          geom_map(map=usa,color="black",size=1)+
                          expand_limits(x=final_data$x,y=final_data$y)+
                          coord_map()+
-                         geom_point()+
+                         geom_point(color='red')+
                          xlim(NY$lon-10,NY$lon+10)+
                          ylim(NY$lat-10,NY$lat+10)
                 
