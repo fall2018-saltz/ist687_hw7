f41775a3859369f5a7c891d88e09e950e34ddafb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:24:06 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -12,6 +12,8 @@ final_data <-merge(clean_data,arrests,by="stateName")
 #str(arrests)
 #Adding area column to the final data
 final_data$area <- state.area
+#Adding longitude column to the final data
 final_data$x <-state.center$x
+#Adding latitude column to the final data
 final_data$y <- state.center$y
 
